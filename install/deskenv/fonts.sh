d00d6a870f87769ab3e8268e9b29a4ce9a7d9359
Calum Price <calumprice.dev@gmail.com>
2025-12-05 18:40:28 +0000

    added nerd-font-symbols and emoji to fix glyphs issue in wezterm

--- install/deskenv/fonts.sh
+++ install/deskenv/fonts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd 
+paru -S --noconfirm ttf-font-awesome ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd noto-fonts-emoji ttf-nerd-fonts-symbols
