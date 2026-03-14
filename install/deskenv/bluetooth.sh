e2216e60d37ca7464b62f3da31b4797cb160d554
Calum Price <calumprice.dev@gmail.com>
2026-03-14 12:03:56 +0000

    adding overskride to bluetooth

--- install/deskenv/bluetooth.sh
+++ install/deskenv/bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install the blueberry bluetooth package
-paru -S --noconfirm blueberry
+paru -S --noconfirm blueberry overskride
 
 # enable the bluetooth service 
 sudo systemctl enable --now bluetooth.service
